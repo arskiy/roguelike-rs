d7f70121da12e71ec0064689ef560c1b7932122e
Alice Micheloni <alicemicheloni@tutanota.com>
2020-04-05 16:06:17 -0300

    AI working

--- src/ai.rs
+++ src/ai.rs
@@ -4,13 +4,18 @@ use crate::object;
 use crate::object::Object;
 
 pub fn take_turn(monster_id: usize, game: &mut Game) {
-    let objects = game.graphics.objects.borrow();
     // only move if close
-    if objects[monster_id].distance_to(&objects[PLAYER]) <= 10.0 {
-        let (monster_x, monster_y) = objects[monster_id].pos();
-        if objects[monster_id].distance_to(&objects[PLAYER]) >= 2.0 {
+    if game.graphics.objects.borrow()[monster_id]
+        .distance_to(&game.graphics.objects.borrow()[PLAYER])
+        <= 10.0
+    {
+        // let (monster_x, monster_y) = game.graphics.objects.borrow()[monster_id].pos();
+        if game.graphics.objects.borrow()[monster_id]
+            .distance_to(&game.graphics.objects.borrow()[PLAYER])
+            >= 2.0
+        {
             // move towards player if far away
-            let (player_x, player_y) = objects[PLAYER].pos();
+            let (player_x, player_y) = game.graphics.objects.borrow()[PLAYER].pos();
             object::move_towards(
                 monster_id,
                 player_x,
@@ -18,16 +23,15 @@ pub fn take_turn(monster_id: usize, game: &mut Game) {
                 &game.map,
                 &mut game.graphics.objects.borrow_mut(),
             );
-        } else if objects[PLAYER].fighter.map_or(false, |f| f.hp > 0) {
+        } else if game.graphics.objects.borrow()[PLAYER]
+            .fighter
+            .map_or(false, |f| f.hp > 0)
+        {
             // close enough, attack! (if the player is still alive.)
-            let monster = &objects[monster_id];
-            game.graphics.statuses.push(Status::new(
-                format!(
-                    "The attack of the {} bounces off your shiny metal armor!",
-                    monster.name
-                ),
-                1,
-            ));
+            let monster = &game.graphics.objects.borrow()[monster_id];
+            game.graphics
+                .statuses
+                .push(Status::new(format!("{} attacks you!", monster.name), 1));
         }
     }
 }
